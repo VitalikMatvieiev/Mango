fce510bd8a50178d636508fb8e5dac7ddd79d7f0
Vitalii Matvieiev <vmatvi@softserveinc.com>
2022-04-21 21:45:50 +0300

    Add project files.

---
+++ Mango.Services.ProductAPI/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Mango.Services.ProductAPI.DbContext.Models;
+
+namespace Mango.Services.ProductAPI.DbContext
+{
+    using Microsoft.EntityFrameworkCore;
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Mango.Services.ProductAPI/DbContext/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ProductAPI.DbContext.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+        [Range(1,1000)]
+        public double Price { get; set; }
+
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
